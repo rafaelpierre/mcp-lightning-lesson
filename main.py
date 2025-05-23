61d44121e80e0c744be2decf42faff0e6caedcf4
Rafael Pierre <rafaelpierre@github.com>
2025-05-23 15:58:08 +0200

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+from agents.mcp import MCPServerStdio
+import asyncio
+from datetime import datetime
+from agents import Agent, Runner, function_tool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@function_tool
+async def get_time() -> str:
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+mcp_fetch = MCPServerStdio(params = {
+    "command": "uvx",
+    "args": ["mcp-server-fetch"]
+})
+
+async def async_main():
+    async with mcp_fetch:
+        agent = Agent(
+            name = "assistant",
+            model="gpt-4.1-mini",
+            mcp_servers = [mcp_fetch],
+            tools = [get_time]
+        )
+
+        result = await Runner.run(
+            agent,
+            """
+                You are a helpful assistant. Please look at the WHAT, WHY and HOW sections below to fulfill the user's request.
+                ### WHAT
+                    Please get the weather forecast for Hintertux, Austria.
+                ### HOW
+                    1. Fetch the data for this location from www.meteoblue.com website.
+                    2. Get the current time.
+                    3. Return the results as JSON payload.
+                    4. Just return the result as structured JSON data, no other text.
+                ### WHY
+                    I wanna go snowboarding, but only if conditions allow it.
+            """
+        )
+
+        print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(async_main())
